fcfddc881250f9c314821973634c046d8db08c0d
Ertuğrul Emre Cihan <cihanertugrulemre368@gmail.com>
2021-01-17 20:44:39 +0300

    Add project files.

---
+++ Paint/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace Paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.grpColor = new System.Windows.Forms.GroupBox();
+            this.white = new System.Windows.Forms.Button();
+            this.black = new System.Windows.Forms.Button();
+            this.green = new System.Windows.Forms.Button();
+            this.blue = new System.Windows.Forms.Button();
+            this.red = new System.Windows.Forms.Button();
+            this.grpActivateColor = new System.Windows.Forms.GroupBox();
+            this.btnActivateColor = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.grpColor.SuspendLayout();
+            this.grpActivateColor.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.Location = new System.Drawing.Point(-1, 27);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(949, 421);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // grpColor
+            // 
+            this.grpColor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpColor.Controls.Add(this.white);
+            this.grpColor.Controls.Add(this.black);
+            this.grpColor.Controls.Add(this.green);
+            this.grpColor.Controls.Add(this.blue);
+            this.grpColor.Controls.Add(this.red);
+            this.grpColor.Controls.Add(this.grpActivateColor);
+            this.grpColor.Location = new System.Drawing.Point(0, 445);
+            this.grpColor.Name = "grpColor";
+            this.grpColor.Size = new System.Drawing.Size(950, 52);
+            this.grpColor.TabIndex = 1;
+            this.grpColor.TabStop = false;
+            // 
+            // white
+            // 
+            this.white.BackColor = System.Drawing.Color.White;
+            this.white.Location = new System.Drawing.Point(217, 19);
+            this.white.Name = "white";
+            this.white.Size = new System.Drawing.Size(29, 24);
+            this.white.TabIndex = 3;
+            this.white.UseVisualStyleBackColor = false;
+            this.white.Click += new System.EventHandler(this.white_Click);
+            // 
+            // black
+            // 
+            this.black.BackColor = System.Drawing.Color.Black;
+            this.black.Location = new System.Drawing.Point(182, 19);
+            this.black.Name = "black";
+            this.black.Size = new System.Drawing.Size(29, 24);
+            this.black.TabIndex = 3;
+            this.black.UseVisualStyleBackColor = false;
+            this.black.Click += new System.EventHandler(this.black_Click);
+            // 
+            // green
+            // 
+            this.green.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.green.Location = new System.Drawing.Point(147, 19);
+            this.green.Name = "green";
+            this.green.Size = new System.Drawing.Size(29, 24);
+            this.green.TabIndex = 3;
+            this.green.UseVisualStyleBackColor = false;
+            this.green.Click += new System.EventHandler(this.green_Click);
+            // 
+            // blue
+            // 
+            this.blue.BackColor = System.Drawing.Color.Blue;
+            this.blue.Location = new System.Drawing.Point(112, 19);
+            this.blue.Name = "blue";
+            this.blue.Size = new System.Drawing.Size(29, 24);
+            this.blue.TabIndex = 3;
+            this.blue.UseVisualStyleBackColor = false;
+            this.blue.Click += new System.EventHandler(this.blue_Click);
+            // 
+            // red
+            // 
+            this.red.BackColor = System.Drawing.Color.Red;
+            this.red.Location = new System.Drawing.Point(77, 19);
+            this.red.Name = "red";
+            this.red.Size = new System.Drawing.Size(29, 24);
+            this.red.TabIndex = 3;
+            this.red.UseVisualStyleBackColor = false;
+            this.red.Click += new System.EventHandler(this.red_Click);
+            // 
+            // grpActivateColor
+            // 
+            this.grpActivateColor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpActivateColor.Controls.Add(this.btnActivateColor);
+            this.grpActivateColor.Location = new System.Drawing.Point(0, 9);
+            this.grpActivateColor.Name = "grpActivateColor";
+            this.grpActivateColor.Size = new System.Drawing.Size(59, 43);
+            this.grpActivateColor.TabIndex = 2;
+            this.grpActivateColor.TabStop = false;
+            // 
+            // btnActivateColor
+            // 
+            this.btnActivateColor.Location = new System.Drawing.Point(12, 10);
+            this.btnActivateColor.Name = "btnActivateColor";
+            this.btnActivateColor.Size = new System.Drawing.Size(29, 24);
+            this.btnActivateColor.TabIndex = 3;
+            this.btnActivateColor.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(948, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 500);
+            this.Controls.Add(this.grpColor);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Draw Picture";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.grpColor.ResumeLayout(false);
+            this.grpActivateColor.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.GroupBox grpColor;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button black;
+        private System.Windows.Forms.Button green;
+        private System.Windows.Forms.Button blue;
+        private System.Windows.Forms.Button red;
+        private System.Windows.Forms.GroupBox grpActivateColor;
+        private System.Windows.Forms.Button btnActivateColor;
+        private System.Windows.Forms.Button white;
+    }
+}
+
---
+++ Paint/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class Form1 : Form
+    {
+        public Point _startPoint;
+        private Graphics _graphics;
+        private Pen _pen;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            this._graphics = pictureBox.CreateGraphics();
+            this._pen = new Pen(Color.Black, 2);
+
+            btnActivateColor.BackColor = Color.Black;
+
+            pictureBox.MouseDown += PictureBox_MouseDown;
+            pictureBox.MouseMove += PictureBox_MouseMove;
+            pictureBox.MouseUp += PictureBox_MouseUp;
+        }
+
+        private void PictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            this._startPoint = Point.Empty;
+        }
+
+        private void PictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!this._startPoint.IsEmpty)
+            {
+                this._graphics.DrawLine(this._pen, this._startPoint, e.Location);
+                this._startPoint = e.Location;
+            }
+        }
+
+        private void PictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+            this._startPoint = e.Location;
+        }
+
+        private void red_Click(object sender, EventArgs e)
+        {
+            btnActivateColor.BackColor = Color.Red;
+            this._pen = new Pen(Color.Red, 2);
+        }
+
+        private void blue_Click(object sender, EventArgs e)
+        {
+            btnActivateColor.BackColor = Color.Blue;
+            this._pen = new Pen(Color.Blue, 2);
+        }
+
+        private void green_Click(object sender, EventArgs e)
+        {
+            btnActivateColor.BackColor = Color.Green;
+            this._pen = new Pen(Color.Green, 2);
+        }
+
+        private void black_Click(object sender, EventArgs e)
+        {
+            btnActivateColor.BackColor = Color.Black;
+            this._pen = new Pen(Color.Black, 2);
+        }
+
+        private void white_Click(object sender, EventArgs e)
+        {
+            btnActivateColor.BackColor = Color.White;
+            this._pen = new Pen(Color.White, 2);
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var fileFullName = $"c:\\Draw\\Draw_Picture_{DateTime.Now.Ticks}.png";
+
+            var bitmaps = new Bitmap(pictureBox.Width, pictureBox.Height);
+            var graphics = Graphics.FromImage(bitmaps);
+
+            var rectangle = pictureBox.RectangleToScreen(pictureBox.ClientRectangle);
+            graphics.CopyFromScreen(rectangle.Location, Point.Empty, pictureBox.Size);
+
+            bitmaps.Save(fileFullName);
+
+            Process.Start(fileFullName);
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            base.Close();
+        }
+    }
+}
